0f54f71424a539b0bab86d78cd82888a8aa226bd
Yuanhe Zhang <108461965+YuanheZ@users.noreply.github.com>
2026-02-07 00:35:57 +0000

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -34,9 +34,6 @@ require mathlib from git
 lean_lib «SLT» where
   globs := #[.submodules `SLT]
 
-lean_lib «Clt» where
-  globs := #[.submodules `Clt]
-
 meta if get_config? env = some "dev" then
 require «doc-gen4» from git
   "https://github.com/leanprover/doc-gen4" @ "main"
